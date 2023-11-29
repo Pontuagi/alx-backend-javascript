3da1d61735ab302661ae4288d355ba2195f46ea9
Pontuagi <benepontuagi@gmail.com>
2023-11-30 00:28:11 +0300

    Throw an error correction 2

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,14 +1,13 @@
 export default function guardrail(mathFunction) {
-  const queue = [];
+  const queue = []; // Initialize an empty array to store results or errors
 
   try {
-    const result = mathFunction();
-    queue.push(result);
+    queue.push(mathFunction()); // Try executing mathFunction and push its result into the queue
   } catch (error) {
-    queue.push(String(error));
+    queue.push(String(error)); // If an error occurs, convert it to a string and push into the queue
   } finally {
-    queue.push('Guardrail was processed');
+    queue.push('Guardrail was processed'); // Add a message to indicate the guardrail processing
   }
-
+  // Return the populated queue containing results or errors and the processing message
   return queue;
 }
