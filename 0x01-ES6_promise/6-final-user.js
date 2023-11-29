5db164034504336194726838fca4b7efd726380a
Pontuagi <benepontuagi@gmail.com>
2023-11-29 23:45:27 +0300

    handling Multiple Promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+
+  const statuses = promises.map((promise) => promise
+    .then((value) => ({ status: 'fulfilled', value }))
+    .catch((error) => ({ status: 'rejected', value: error })));
+
+  return Promise.all(statuses);
+}
