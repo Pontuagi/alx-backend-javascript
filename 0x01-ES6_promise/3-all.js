8031cb7c72bfa4d5fc39663f6c3594036313ca98
Pontuagi <benepontuagi@gmail.com>
2023-11-30 03:54:57 +0300

    Multiple promises correction 2

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,7 +6,5 @@ export default function handleProfileSignup() {
     .then((result) => {
       console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
-    .catch(() => {
-      console.log('Signup system offline:');
-    });
+    .catch(() => console.log('Signup system offline:'));
 }
