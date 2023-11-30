9d78cae72239d0b753c5f8df06e2bd18b8817017
Pontuagi <benepontuagi@gmail.com>
2023-11-30 04:10:36 +0300

    throw error/try catch correction 2

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,4 +1,8 @@
 export default function divideFunction(numerator, denominator) {
+  if (typeof numerator !== 'number' || typeof denominator !== 'number') {
+    throw new Error('Both numerator and denominator must be numbers');
+  }
+
   if (denominator === 0) {
     throw Error('Cannot divide by 0');
   }
