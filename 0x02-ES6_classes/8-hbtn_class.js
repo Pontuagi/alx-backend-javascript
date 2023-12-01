8c3d76eb1b214e76b9b38d8f83953e21d6f4274e
Pontuagi <benepontuagi@gmail.com>
2023-12-01 03:47:38 +0300

    primitive Holberton class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,39 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size || 0;
+    this._location = location || '';
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(newSize) {
+    if (typeof newSize === 'number') {
+      this._size = newSize;
+    } else {
+      throw new TypeError('Size must be a number.');
+    }
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(newLocation) {
+    if (typeof newLocation === 'string') {
+      this._location = newLocation;
+    } else {
+      throw new TypeError('Location must be a string.');
+    }
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    } if (hint === 'string') {
+      return this._location;
+    }
+    return null;
+  }
+}
