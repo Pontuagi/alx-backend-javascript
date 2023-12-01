33c333fcff7da1279acb6ac21648fb3c170b244e
Pontuagi <benepontuagi@gmail.com>
2023-12-01 03:11:59 +0300

    Buildingclass correction

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -8,6 +8,15 @@ export default class Building {
     return this._sqft;
   }
 
+  // setter for sqft
+  set sqft(newSqft) {
+    if (typeof newSqft === 'number') {
+      this._sqft = newSqft;
+    } else {
+      throw new TypeError('Square footage must be a number.');
+    }
+  }
+
   // Abstract method that needs to be implemented by subclasses
   // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
