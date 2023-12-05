d631429dc35fe8367e50ec644b1f7b53df47c74f
Pontuagi <benepontuagi@gmail.com>
2023-12-05 15:57:17 +0300

    Clean set correction

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,15 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  if (startString.trim() === '') {
     return '';
   }
 
   const filteredValues = Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length));
+
+  if (startString === '') {
+    return Array.from(set).join('-');
+  }
 
   return filteredValues.join('-');
 }
