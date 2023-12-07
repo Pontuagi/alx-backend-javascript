f0927dd13815536f0558af0b65b22c1c7397abcc
Pontuagi <benepontuagi@gmail.com>
2023-12-07 03:13:07 +0300

    task 1 student class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// Teacher interface.
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -16,3 +17,58 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// New interface that extends Teacher interface
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+// Interface for print teacher function
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
+  const firstInitial = firstName.charAt(0);
+  return `${firstInitial}. ${lastName}`;
+};
+
+// Interface describing the constructor of StudentClass
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+// Interface describing the StudentClass
+interface StudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// StudentClass implementation
+class Student implements StudentClass {
+  private readonly firstName: string;
+  private readonly lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
